f56c2f9edde8b22c63f0ae74f23c3ebcf4bf863c
Geistliche Lieder <geistliche-lieder@proton.me>
2022-12-17 19:13:59 +0000

    Updates

--- _app/immutable/chunks/1-10d56a11.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-bb7edc01.js";export{t as component};
--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,28 @@
 const l = [
-  "/_app/immutable/start-590fb370.js",
+  "/_app/immutable/start-52675eea.js",
   "/_app/immutable/components/pages/_layout.svelte-c420e78e.js",
   "/_app/immutable/assets/_layout-432cae01.css",
-  "/_app/immutable/components/error.svelte-bb7edc01.js",
+  "/_app/immutable/components/error.svelte-9c5d4261.js",
   "/_app/immutable/components/pages/_page.svelte-da207fb4.js",
   "/_app/immutable/assets/_page-51bfa698.css",
-  "/_app/immutable/components/pages/song/_songId_/_page.svelte-254fee4f.js",
-  "/_app/immutable/components/pages/song/_songId_/_verseIndex_/_page.svelte-b8150fd9.js",
+  "/_app/immutable/components/pages/song/_songId_/_page.svelte-2a4d7fdf.js",
+  "/_app/immutable/components/pages/song/_songId_/_verseIndex_/_page.svelte-63673c12.js",
   "/_app/immutable/modules/pages/_layout.ts-b8ee4d7c.js",
   "/_app/immutable/modules/pages/song/_songId_/_page.ts-9232d639.js",
   "/_app/immutable/modules/pages/song/_songId_/_verseIndex_/_page.ts-83245c77.js",
-  "/_app/immutable/chunks/singletons-af80a951.js",
+  "/_app/immutable/chunks/singletons-bb482e49.js",
   "/_app/immutable/chunks/index-37370bd0.js",
   "/_app/immutable/chunks/song-9a1cbec6.js",
   "/_app/immutable/chunks/_layout-1daba58d.js",
-  "/_app/immutable/chunks/stores-f92e4456.js",
-  "/_app/immutable/chunks/navigation-21b9cb4d.js",
+  "/_app/immutable/chunks/stores-dda73cf5.js",
+  "/_app/immutable/chunks/navigation-323b07eb.js",
   "/_app/immutable/chunks/_page-1c03c40a.js",
   "/_app/immutable/chunks/_page-022daf14.js",
   "/_app/immutable/chunks/0-9c22ddb5.js",
-  "/_app/immutable/chunks/1-10d56a11.js",
+  "/_app/immutable/chunks/1-a67eff47.js",
   "/_app/immutable/chunks/2-1fd380bc.js",
-  "/_app/immutable/chunks/3-ca14f5d8.js",
-  "/_app/immutable/chunks/4-f2ef333b.js"
+  "/_app/immutable/chunks/3-85a551cb.js",
+  "/_app/immutable/chunks/4-44a85765.js"
 ], u = [
   "/.nojekyll",
   "/android-chrome-192x192.png",
@@ -43,7 +43,7 @@ const l = [
   "/safari-pinned-tab.svg",
   "/site.webmanifest",
   "/songs.json"
-], o = "1671304240007", t = self, p = `cache${o}`, i = l.concat(u), h = new Set(i);
+], o = "1671304433870", t = self, p = `cache${o}`, i = l.concat(u), h = new Set(i);
 t.addEventListener("install", (s) => {
   s.waitUntil(
     caches.open(p).then((e) => e.addAll(i)).then(() => {
