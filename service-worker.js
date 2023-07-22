2cb5f4cf66acdd92234161e9e8ade54c772f361c
Geistliche Lieder <geistliche-lieder@proton.me>
2023-07-22 21:39:48 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,23 @@
 const s = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), r = [
-  s + "/_app/immutable/entry/app.59874e8b.js",
+  s + "/_app/immutable/entry/app.5ce43041.js",
   s + "/_app/immutable/nodes/0.d520ca0d.js",
-  s + "/_app/immutable/nodes/1.cb563f83.js",
+  s + "/_app/immutable/nodes/1.19a1bfb5.js",
   s + "/_app/immutable/nodes/2.10106310.js",
   s + "/_app/immutable/assets/app.3e1744fb.css",
   s + "/_app/immutable/assets/3.625db5c3.css",
-  s + "/_app/immutable/nodes/3.c31588b6.js",
-  s + "/_app/immutable/nodes/4.b70868ae.js",
+  s + "/_app/immutable/nodes/3.a3bf7227.js",
+  s + "/_app/immutable/nodes/4.af339ded.js",
   s + "/_app/immutable/assets/5.f4b2c993.css",
-  s + "/_app/immutable/nodes/5.89e7284a.js",
+  s + "/_app/immutable/nodes/5.a1b9cf23.js",
   s + "/_app/immutable/chunks/TitleBar.f5b25ff0.js",
   s + "/_app/immutable/chunks/index.4e284ba1.js",
   s + "/_app/immutable/chunks/index.eab26d42.js",
   s + "/_app/immutable/chunks/scheduler.c7c6d1ea.js",
-  s + "/_app/immutable/chunks/singletons.75c3f268.js",
-  s + "/_app/immutable/chunks/song.114e1fbd.js",
+  s + "/_app/immutable/chunks/singletons.b111a3ee.js",
+  s + "/_app/immutable/chunks/song.b7df671b.js",
   s + "/_app/immutable/chunks/store.d78c978e.js",
-  s + "/_app/immutable/chunks/stores.d5ff2d09.js",
-  s + "/_app/immutable/entry/start.2254dbaa.js"
+  s + "/_app/immutable/chunks/stores.f2bfaed4.js",
+  s + "/_app/immutable/entry/start.fdc27e91.js"
 ], h = [
   s + "/.nojekyll",
   s + "/android-chrome-192x192.png",
@@ -39,7 +39,7 @@ const s = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), r
   s + "/safari-pinned-tab.svg",
   s + "/site.webmanifest",
   s + "/songs.json"
-], o = "1689889199333", n = self, p = `cache${o}`, l = r.concat(h), u = new Set(l);
+], o = "1690061981290", n = self, p = `cache${o}`, l = r.concat(h), d = new Set(l);
 n.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open(p).then((t) => t.addAll(l)).then(() => {
@@ -56,7 +56,7 @@ n.addEventListener("activate", (e) => {
     })
   );
 });
-async function d(e) {
+async function u(e) {
   const t = await caches.open(`offline${o}`);
   try {
     const a = await fetch(e);
@@ -71,8 +71,8 @@ async function d(e) {
 n.addEventListener("fetch", (e) => {
   if (e.request.method !== "GET" || e.request.headers.has("range"))
     return;
-  const t = new URL(e.request.url), a = t.protocol.startsWith("http"), c = t.hostname === self.location.hostname && t.port !== self.location.port, i = t.host === self.location.host && u.has(t.pathname), m = e.request.cache === "only-if-cached" && !i;
+  const t = new URL(e.request.url), a = t.protocol.startsWith("http"), c = t.hostname === self.location.hostname && t.port !== self.location.port, i = t.host === self.location.host && d.has(t.pathname), m = e.request.cache === "only-if-cached" && !i;
   a && !c && !m && e.respondWith(
-    (async () => i && await caches.match(e.request) || d(e.request))()
+    (async () => i && await caches.match(e.request) || u(e.request))()
   );
 });
