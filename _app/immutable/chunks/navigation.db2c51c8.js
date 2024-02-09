6245111aabc6542060f5958994bde2323b47649b
Geistliche Lieder <geistliche-lieder@proton.me>
2024-02-09 23:25:28 +0000

    Updates

---
+++ _app/immutable/chunks/navigation.db2c51c8.js
@@ -0,0 +1,1 @@
+import{j as o}from"./singletons.04960d53.js";const e=o("goto");export{e as g};
--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,27 @@
 const s = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), h = [
-  s + "/_app/immutable/entry/app.333a776b.js",
-  s + "/_app/immutable/nodes/0.b84d566a.js",
-  s + "/_app/immutable/nodes/1.8b22467d.js",
+  s + "/_app/immutable/entry/app.c2603638.js",
+  s + "/_app/immutable/nodes/0.8f1dcff0.js",
+  s + "/_app/immutable/nodes/1.fd776e79.js",
   s + "/_app/immutable/assets/2.625db5c3.css",
-  s + "/_app/immutable/nodes/2.92ce09ce.js",
-  s + "/_app/immutable/nodes/3.203505f1.js",
-  s + "/_app/immutable/nodes/4.066178ed.js",
-  s + "/_app/immutable/nodes/5.ca200842.js",
+  s + "/_app/immutable/nodes/2.b513a88a.js",
+  s + "/_app/immutable/nodes/3.41cef64d.js",
+  s + "/_app/immutable/nodes/4.063558b3.js",
+  s + "/_app/immutable/nodes/5.ba2c989b.js",
   s + "/_app/immutable/assets/SettingsPage.f4b2c993.css",
   s + "/_app/immutable/chunks/SettingsPage.e155967a.js",
-  s + "/_app/immutable/chunks/app.4ebcbc34.js",
-  s + "/_app/immutable/assets/app.b78dfdbd.css",
+  s + "/_app/immutable/chunks/app.4566154c.js",
+  s + "/_app/immutable/assets/app.65daea4a.css",
   s + "/_app/immutable/chunks/common.98f050d8.js",
   s + "/_app/immutable/chunks/index.2f5ba9d6.js",
   s + "/_app/immutable/chunks/index.52f962b4.js",
   s + "/_app/immutable/chunks/music.86076db2.js",
-  s + "/_app/immutable/chunks/navigation.15f6515a.js",
+  s + "/_app/immutable/chunks/navigation.db2c51c8.js",
   s + "/_app/immutable/chunks/scheduler.143b6de7.js",
-  s + "/_app/immutable/chunks/singletons.9ea987a7.js",
+  s + "/_app/immutable/chunks/singletons.04960d53.js",
   s + "/_app/immutable/chunks/song.8d2a36b6.js",
   s + "/_app/immutable/chunks/store.8dced48f.js",
-  s + "/_app/immutable/chunks/stores.e5d2bb9d.js",
-  s + "/_app/immutable/entry/start.4a5ee279.js"
+  s + "/_app/immutable/chunks/stores.3863c576.js",
+  s + "/_app/immutable/entry/start.6c81bd28.js"
 ], r = [
   s + "/.nojekyll",
   s + "/android-chrome-192x192.png",
@@ -45,7 +45,7 @@ const s = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), h
   s + "/safari-pinned-tab.svg",
   s + "/site.webmanifest",
   s + "/songs.json"
-], o = "1705096588446", n = self, p = `cache${o}`, m = h.concat(r), u = new Set(m);
+], o = "1707521050533", n = self, p = `cache${o}`, m = h.concat(r), u = new Set(m);
 n.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open(p).then((a) => a.addAll(m)).then(() => {
