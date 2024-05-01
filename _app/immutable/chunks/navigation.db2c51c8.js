278b60178602c19121e09def948f6403f09fe06e
Geistliche Lieder <geistliche-lieder@proton.me>
2024-05-01 15:27:11 +0000

    Updates

--- _app/immutable/chunks/navigation.db2c51c8.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./singletons.04960d53.js";const e=o("goto");export{e as g};
--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,27 @@
 const s = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), h = [
-  s + "/_app/immutable/entry/app.c2603638.js",
-  s + "/_app/immutable/nodes/0.8f1dcff0.js",
-  s + "/_app/immutable/nodes/1.fd776e79.js",
+  s + "/_app/immutable/entry/app.d9751f73.js",
+  s + "/_app/immutable/nodes/0.585a0c88.js",
+  s + "/_app/immutable/nodes/1.6b10952b.js",
   s + "/_app/immutable/assets/2.625db5c3.css",
-  s + "/_app/immutable/nodes/2.b513a88a.js",
-  s + "/_app/immutable/nodes/3.41cef64d.js",
-  s + "/_app/immutable/nodes/4.063558b3.js",
-  s + "/_app/immutable/nodes/5.ba2c989b.js",
+  s + "/_app/immutable/nodes/2.ee6bd69f.js",
+  s + "/_app/immutable/nodes/3.767c1f96.js",
+  s + "/_app/immutable/nodes/4.e4cda6c4.js",
+  s + "/_app/immutable/nodes/5.00f53966.js",
   s + "/_app/immutable/assets/SettingsPage.f4b2c993.css",
-  s + "/_app/immutable/chunks/SettingsPage.e155967a.js",
-  s + "/_app/immutable/chunks/app.4566154c.js",
-  s + "/_app/immutable/assets/app.65daea4a.css",
+  s + "/_app/immutable/chunks/SettingsPage.168c3ffd.js",
+  s + "/_app/immutable/chunks/app.c94666de.js",
+  s + "/_app/immutable/assets/app.be306f78.css",
   s + "/_app/immutable/chunks/common.98f050d8.js",
   s + "/_app/immutable/chunks/index.2f5ba9d6.js",
   s + "/_app/immutable/chunks/index.52f962b4.js",
   s + "/_app/immutable/chunks/music.86076db2.js",
-  s + "/_app/immutable/chunks/navigation.db2c51c8.js",
+  s + "/_app/immutable/chunks/navigation.1b771e2d.js",
   s + "/_app/immutable/chunks/scheduler.143b6de7.js",
-  s + "/_app/immutable/chunks/singletons.04960d53.js",
+  s + "/_app/immutable/chunks/singletons.1acf225d.js",
   s + "/_app/immutable/chunks/song.8d2a36b6.js",
   s + "/_app/immutable/chunks/store.8dced48f.js",
-  s + "/_app/immutable/chunks/stores.3863c576.js",
-  s + "/_app/immutable/entry/start.6c81bd28.js"
+  s + "/_app/immutable/chunks/stores.e199c575.js",
+  s + "/_app/immutable/entry/start.471f45b9.js"
 ], r = [
   s + "/.nojekyll",
   s + "/android-chrome-192x192.png",
@@ -45,40 +45,40 @@ const s = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), h
   s + "/safari-pinned-tab.svg",
   s + "/site.webmanifest",
   s + "/songs.json"
-], o = "1707521050533", n = self, p = `cache${o}`, m = h.concat(r), u = new Set(m);
+], o = "1714577159717", n = self, p = `cache${o}`, m = h.concat(r), u = new Set(m);
 n.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open(p).then((a) => a.addAll(m)).then(() => {
+    caches.open(p).then((t) => t.addAll(m)).then(() => {
       n.skipWaiting();
     })
   );
 });
 n.addEventListener("activate", (e) => {
   e.waitUntil(
-    caches.keys().then(async (a) => {
-      for (const t of a)
-        t !== p && await caches.delete(t);
+    caches.keys().then(async (t) => {
+      for (const a of t)
+        a !== p && await caches.delete(a);
       n.clients.claim();
     })
   );
 });
 async function d(e) {
-  const a = await caches.open(`offline${o}`);
+  const t = await caches.open(`offline${o}`);
   try {
-    const t = await fetch(e);
-    return a.put(e, t.clone()), t;
-  } catch (t) {
-    const c = await a.match(e);
+    const a = await fetch(e);
+    return t.put(e, a.clone()), a;
+  } catch (a) {
+    const c = await t.match(e);
     if (c)
       return c;
-    throw t;
+    throw a;
   }
 }
 n.addEventListener("fetch", (e) => {
   if (e.request.method !== "GET" || e.request.headers.has("range"))
     return;
-  const a = new URL(e.request.url), t = a.protocol.startsWith("http"), c = a.hostname === self.location.hostname && a.port !== self.location.port, i = a.host === self.location.host && u.has(a.pathname), l = e.request.cache === "only-if-cached" && !i;
-  t && !c && !l && e.respondWith(
+  const t = new URL(e.request.url), a = t.protocol.startsWith("http"), c = t.hostname === self.location.hostname && t.port !== self.location.port, i = t.host === self.location.host && u.has(t.pathname), l = e.request.cache === "only-if-cached" && !i;
+  a && !c && !l && e.respondWith(
     (async () => i && await caches.match(e.request) || d(e.request))()
   );
 });
